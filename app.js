3fc1b8fbf8080c53fcd484921903b8f39f26cf06
Sumbal Zaheer <sumbalzshaikh@gmail.com>
2023-10-01 03:48:17 -0700

    Java Assignment

---
+++ app.js
@@ -0,0 +1,237 @@
+// *****************question1***********
+var fisrtname = "Sumbal";
+alert(fisrtname);
+
+// *****************question1***********
+var lastname = "Zaheer";
+alert(lastname);
+
+// *****************question1***********
+var email = "sumbalzshaikh@gmail.com";
+alert(email);
+
+// *****************question1***********
+var phonenumber = "03212986776";
+alert(phonenumber);
+
+// *****************question1***********
+var password = "xyw";
+alert(password);
+
+// *****************question1***********
+alert("You're learning JavaScript!");
+
+// *****************question1***********
+alert("Hello World");
+
+// *****************chapter2***********
+var firstName;
+// *****************chapter2***********
+var lastNmae;
+lastNmae = "Zaheer";
+alert(lastNmae);
+// *****************chapter2***********
+
+var teamName = "DareDevil";
+alert(teamName);
+
+// *****************chapter2***********
+var bestMan = "Carlie";
+var bestMan = "Chaplan";
+alert(bestMan);
+
+// *****************chapter3***********
+// caseQty = 144;
+// // *****************chapter3***********
+// var num = 9;
+// // *****************chapter3***********
+// var num3 = number1 + number2;
+
+// var sum;
+// sum = 3 + 4;
+// *****************chapter3***********
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal);
+// *****************chapter3***********
+var eg = 4;
+eg = eg + 2;
+alert(eg);
+// *****************chapter4***********
+// var product_cost = 3.45;
+// var nameOfBand;
+// var firstNum = 3;
+// var sumbal_zaheer;
+// // ************************legal**************
+// var sumbal123;
+// var sumbalZaheer;
+// var sumbal_zaheer;
+// // ************************illegal**************
+// var 123num;
+// var num@;
+// var Num;
+// var last name;
+// ************************chapter5**************
+// modulus and %
+// // 2
+// var largeNum = 1000 * 2000;
+// var subtract = number1 - number2;
+
+// var remainder = 4 % 3;
+alert(2 * 4);
+
+// ************************chapter6**************
+// x++
+// --x
+// value of y is equal to 50
+// value of z is equal to 49
+// var newNum = num--
+// var num1 = num++
+var num = 3;
+num++;
+alert(num);
+// ************************chapter7**************
+// calculatedNum = 14
+// calculatedNum = 24
+// calculatedNum = 24
+// calculatedNum = 18
+// result ( (2 + 2) * (4 + 10));
+// result ( 2 +( 2 * 4 ) + 10);
+// ************************chapter8*************
+// 22
+// "Hello,Dolly"
+// 333
+alert("Pakistan" + " Zindabad");
+// sum = "hi" + 1;
+// var result = variable1 + variable2;
+// ************************chapter9*************
+var firstname = prompt("Enter first name");
+var yourcountry = prompt("Country?", "China");
+// var yourName = prompt("Enter Your Name");
+var spec = prompt("Your species?", "human");
+var firstvariable = "your last name";
+var secondvariable = "only letters";
+var thirdvariable = prompt(firstvariable, secondvariable);
+
+// ************************chapter10*************
+// var city = "Karachi"
+// if (city == "Karachi") {
+// console.log("The City OF Lights")
+var zipCode = prompt("enter your ZipCode");
+if (zipCode == 10010){
+    alert("Karachi");
+}
+else{alert("enter your correct ZipCode");}
+
+// ************************chapter11*************
+// if(yourAge !== 23){
+//     alert("you are not eligible");
+// }
+
+// if(numb1 >= numb2){ alert("your condition is true");
+
+// }
+// ********************chapter12*****************
+var a = +prompt("enter a number");
+if (a === 10) {
+    alert("a is 10");
+    }
+    else{
+        alert(a);
+    }
+
+// ********************chapter13*****************
+// if(a==b && c==d){}
+// if(name == "hamza" || name == "arsalan" && age < 60){}
+
+// ********************chapter14*****************
+
+if(a===1 && c==="max"){
+    alert("ok");
+}
+var a=2;
+var b=2;
+if(a==b){
+    if (a<=b){
+        alert("condition passes")
+    }
+}
+// **********************chapter15**********************
+var empty[];
+var one[fish];
+var alphabet = ["h","i","j","k"];
+alert(alphabet[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
